29c403a0a62658f68149de1f7866c2c3f968703c
contactinfinitysolutionsllc-ops <contact.infinitysolutionsllc@gmail.com>
2026-09-01 14:41:51 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+/* The deployed Apps Script web app URL. Update this if you ever create a brand
+   new deployment (editing an existing deployment keeps the URL the same). */
+window.PARADOX_API =
+  "https://script.google.com/macros/s/AKfycbwzsjjjlCbK919RgsOHpTjtM09t4Ceq_On9qJnGBcGk32h5HKLBUrUrHcZcwbDKens1Mw/exec";
